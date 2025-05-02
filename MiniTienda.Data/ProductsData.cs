d803db1b294b97548cd268ad7dfb0ad044c93867
ElkinYG <elkinyandun@unicomfacauca.edu.co>
2025-05-01 21:33:09 -0500

    Parte de elkin en donde incluye el users.data y products.data y con una actualizacion del script_creacion_cd en donde ya incliye toda la base sde datos

---
+++ MiniTienda.Data/ProductsData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace MiniTienda.Data
+{
+    internal class ProductsData
+    {
+        Persistence objPer = new Persistence();
+
+        public DataSet showUsers()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectProvidersDDL";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        public bool saveProducts(string _code, string _description, int _quantity, double _price, int _fkCategory, int _fkProvider)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Asignar el nombre del procedimiento almacenado que inserta productos
+            objSelectCmd.CommandText = "spInsertProducts"; // <-- Asegúrate de que este procedimiento exista en MySQL
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agregar los parámetros requeridos por el procedimiento con sus respectivos valores
+            objSelectCmd.Parameters.Add("p_code", MySqlDbType.VarString).Value = _code;               
+            objSelectCmd.Parameters.Add("p_description", MySqlDbType.VarString).Value = _description; 
+            objSelectCmd.Parameters.Add("p_quantity", MySqlDbType.Int32).Value = _quantity;           
+            objSelectCmd.Parameters.Add("p_price", MySqlDbType.Double).Value = _price;                
+            objSelectCmd.Parameters.Add("p_fkcategory", MySqlDbType.Int32).Value = _fkCategory;       
+            objSelectCmd.Parameters.Add("p_fkprovider", MySqlDbType.Int32).Value = _fkProvider;      
+
+            try
+            {
+                // Ejecutar el comando y obtener cuántas filas fueron afectadas
+                row = objSelectCmd.ExecuteNonQuery();
+
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+
+
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool updateProducts(int _id, string _code, string _description, int _quantity, double _price, int _fkCategory, int _fkProvider)
+        {
+            bool executed = false; 
+            int row;  
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Se asigna el nombre del procedimiento almacenado encargado de actualizar productos
+            objSelectCmd.CommandText = "spUpdateProduct"; // <-- Asegúrate de que este procedimiento exista en tu base de datos
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan los parámetros requeridos por el procedimiento almacenado con sus valores correspondientes
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;                      
+            objSelectCmd.Parameters.Add("p_code", MySqlDbType.VarString).Value = _code;              
+            objSelectCmd.Parameters.Add("p_description", MySqlDbType.VarString).Value = _description;
+            objSelectCmd.Parameters.Add("p_quantity", MySqlDbType.Int32).Value = _quantity;          
+            objSelectCmd.Parameters.Add("p_price", MySqlDbType.Double).Value = _price;               
+            objSelectCmd.Parameters.Add("p_fkcategory", MySqlDbType.Int32).Value = _fkCategory;      
+            objSelectCmd.Parameters.Add("p_fkprovider", MySqlDbType.Int32).Value = _fkProvider;      
+
+            try
+            {
+                // Se ejecuta el procedimiento almacenado y se obtiene cuántas filas fueron afectadas
+                row = objSelectCmd.ExecuteNonQuery();
+
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // Si ocurre una excepción, se muestra el mensaje de error en consola
+                Console.WriteLine("Error " + e.ToString());
+            }
+
+            objPer.closeConnection();
+            return executed;
+        }
+
+
+
+    }
+}
---
+++ MiniTienda.Data/UsersData.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace MiniTienda.Data
+{
+    internal class UsersData
+    {
+        Persistence objPer = new Persistence();
+
+        public DataSet showUsers()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectProvidersDDL";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        public bool saveUsers(string _mail, string _password, string _salt, string _state)
+        {
+            bool executed = false;  // Variable que indica si la operación fue exitosa
+            int row;  // Almacena el número de filas afectadas por la ejecución del comando
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Se indica el nombre del procedimiento almacenado a ejecutar
+            objSelectCmd.CommandText = "spInsertUsers"; // <-- Aquí debe ir el nombre exacto del procedimiento en MySQL
+
+            // Se indica que el comando es un procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan los parámetros requeridos por el procedimiento almacenado, junto con sus valores
+            objSelectCmd.Parameters.Add("p_mail", MySqlDbType.VarString).Value = _mail;
+            objSelectCmd.Parameters.Add("p_password", MySqlDbType.VarString).Value = _password;
+            objSelectCmd.Parameters.Add("p_salt", MySqlDbType.VarString).Value = _salt;
+            objSelectCmd.Parameters.Add("p_state", MySqlDbType.VarString).Value = _state;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool updateUsers(int _id, string _mail, string _password, string _salt, string _state)
+        {
+            bool executed = false; 
+            int row; 
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+
+            objSelectCmd.CommandText = "spUpdateUsers"; // <-- Este debe coincidir con el nombre del procedimiento en MySQL
+
+            // Indicar que se ejecutará un procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agregar parámetros al comando, que serán enviados al procedimiento almacenado
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;              
+            objSelectCmd.Parameters.Add("p_mail", MySqlDbType.VarString).Value = _mail;     
+            objSelectCmd.Parameters.Add("p_password", MySqlDbType.VarString).Value = _password; 
+            objSelectCmd.Parameters.Add("p_salt", MySqlDbType.VarString).Value = _salt;     
+            objSelectCmd.Parameters.Add("p_state", MySqlDbType.VarString).Value = _state;    
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+
+                // Si se afectó exactamente una fila, se considera una actualización exitosa
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // En caso de error, mostrar el mensaje en la consola
+                Console.WriteLine("Error " + e.ToString());
+            }
+
+
+            objPer.closeConnection();
+            return executed;
+        }
+
+
+    }
+}
